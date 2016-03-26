1a65d8480717dd568f6aaedc4a1eed0124900869
entering <me@eduardooliveira.net>
2016-03-26 23:14:37 +0100

    Minor refactor

--- pingdomexport/load/checks_load.py
+++ pingdomexport/load/checks_load.py
@@ -2,4 +2,4 @@ from pingdomexport.load import checks_output
 
 def load(config, checks):
     # @todo use the config to understand what should be called
-    checks_output.load(checks)
+    checks_output.Output().load(checks)
--- pingdomexport/load/checks_output.py
+++ pingdomexport/load/checks_output.py
@@ -1,16 +1,17 @@
 import tablib
 
-def load(checks):
-    data = tablib.Dataset(headers=['Id', 'Name', 'Created at', 'Status', 'Hostname', 'Type'])
-    for check in checks:
-        data.append(
-            [
-                check['id'],
-                check['name'],
-                check['created'],
-                check['status'],
-                check['hostname'],
-                check['type'],
-            ]
-        )
-    print(data.csv, end="")
+class Output:
+    def load(self, checks):
+        data = tablib.Dataset(headers=['Id', 'Name', 'Created at', 'Status', 'Hostname', 'Type'])
+        for check in checks:
+            data.append(
+                [
+                    check['id'],
+                    check['name'],
+                    check['created'],
+                    check['status'],
+                    check['hostname'],
+                    check['type'],
+                ]
+            )
+        print(data.csv, end="")
