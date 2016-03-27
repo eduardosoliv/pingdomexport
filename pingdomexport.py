91aa5eef5c56deb87bed2283451fad32ec3c78f5
entering <me@eduardooliveira.net>
2016-03-27 18:23:05 +0200

    Add possibility to ask checks results constrainted to an interval

--- pingdomexport.py
+++ pingdomexport.py
@@ -14,12 +14,22 @@ parser.add_argument(
     "--config",
     help="The path to the configuration, if not provided will search config.yml"
 )
+parser.add_argument(
+    "--checks-from",
+    type=int,
+    help="Will returns checks results from the timestamp passed"
+)
+parser.add_argument(
+    "--checks-to",
+    type=int,
+    help="Will returns checks results up to the timestamp passed"
+)
 
 args = parser.parse_args()
 export_type = args.type if args.type else 'all'
 
 try:
-    exporter = export.Export(export_type, args.config)
+    exporter = export.Export(export_type, args.config, args.checks_from, args.checks_to)
     exporter.run()
 except yaml.YAMLError as exc:
     print("Unable to read configuration: " + str(exc))
--- pingdomexport/export.py
+++ pingdomexport/export.py
@@ -2,7 +2,7 @@ from pingdomexport import configuration, pingdom, checks
 from pingdomexport.load import checks_load, checks_results_load
 
 class Export:
-    def __init__(self, export_type = 'all', config_path = None):
+    def __init__(self, export_type = 'all', config_path = None, checks_from = None, checks_to = None):
         if export_type not in ['checks', 'results', 'all']:
             raise ValueError('Invalid export type, must be: checks|results|all')
         config = configuration.Configuration(config_path)
@@ -10,10 +10,16 @@ class Export:
         self.__pingdom = pingdom.Pingdom(config.pingdom_access())
         self.__config = config
         self.__export_type = export_type
+        self.__checks_from = checks_from
+        self.__checks_to = checks_to
 
     def run(self):
         filtered_checks = checks.Picker(self.__config.checks(), self.__pingdom.checks()).filter()
         if self.__export_type in ['all', 'checks']:
             checks_load.Load(self.__config).load(filtered_checks)
         if self.__export_type in ['all', 'results']:
-            checks_results_load.Load(self.__config, self.__pingdom).load(filtered_checks)
+            checks_results_load.Load(self.__config, self.__pingdom).load(
+                filtered_checks,
+                self.__checks_from,
+                self.__checks_to
+            )
